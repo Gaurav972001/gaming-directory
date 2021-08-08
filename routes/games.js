8d618c9b48557bbfc6da8d32962638b0f3dc201a
Gaurav Chauhan <chauhangaurav9721@gmail.com>
2021-08-08 15:05:22 +0530

    created controller

--- routes/games.js
+++ routes/games.js
@@ -1,181 +1,50 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuth } = require('../middleware/auth');
-const Game = require('../models/Game');
-const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif'];
+// const Game = require('../models/Game');
+// const imageMimeTypes = ['image/jpeg', 'image/png', 'image/webp' ,'images/gif'];
+const controller= require('../controller/controller')
 
 //show add page
 //@route get /games/add
-router.get('/add', ensureAuth, (req, res) => {
-  res.render('games/add');
-});
+router.get('/add', ensureAuth, controller.addPage);
 
 //Process add form
 //@route   POST /games
-router.post('/', ensureAuth, async (req, res) => {
-  let newgame = new Game({
-    name: req.body.name,
-    description: req.body.description,
-    status: req.body.status,
-    user: req.user.id,
-    link: req.body.link
-  })
-  try {
-    saveCover(newgame, req.body.cover);
-    // req.body.user = req.user.id
-    // await Game.create(req.body)
-    newgame = await newgame.save();
-    res.redirect('/admin')
-  } catch (err) {
-    console.error(err)
-    res.render('error/form')
-  }
-});
+router.post('/', ensureAuth, controller.addForm);
 
 // Show all games
 // @route   GET /games
-router.get('/', ensureAuth, async (req, res) => {
-  try {
-    const games = await Game.find({ status: 'active' })
-      .populate('user')
-      .sort({ createdAt: 'desc' })
-      .lean();
-
-    res.render('games/index', {
-      games,
-    });
-  } catch (err) {
-    console.error(err);
-    res.render('error/500');
-  }
-});
+router.get('/', ensureAuth, controller.showAllGames);
 
 //Show edit page
 // @route   GET /games/edit/:id
-router.get('/edit/:id', ensureAuth, async (req, res) => {
-  try {
-    const game = await Game.findOne({
-      _id: req.params.id,
-    }).lean()
-
-    if (!game) {
-      return res.render('error/404');
-    }
-
-    if (game.user != req.user.id) {
-      res.redirect('/games');
-    } else {
-      res.render('games/edit', {
-        game,
-      })
-    }
-  } catch (err) {
-    console.error(err);
-    return res.render('error/500');
-  }
-});
+router.get('/edit/:id', controller.showEdit);
 
 //Update game
 // @route   PUT /game/:id
-router.put('/:id', ensureAuth, async (req, res) => {
-  try {
-    let game = await Game.findById(req.params.id).lean()
-
-    if (!game) {
-      return res.render('error/404')
-    }
-    if (game.user != req.user.id) {
-      res.redirect('/games')
-    } else {
-      story = await Game.findOneAndUpdate({ _id: req.params.id },
-        req.body
-    , {
-          new: true,
-          runValidators: true,
-        })
-
-      res.redirect('/admin')
-    }
-  } catch (err) {
-    console.error(err)
-    return res.render('error/500')
-  }
-});
+router.put('/:id', controller.updateGame);
 
 // Delete game
 // @route   DELETE /games/:id
-router.delete('/:id', ensureAuth, async (req, res) => {
-  try {
-    let game = await Game.findById(req.params.id).lean();
-
-    if (!game) {
-      return res.render('error/404');
-    }
-
-    if (game.user != req.user.id) {
-      res.redirect('/games');
-    } else {
-      await Game.remove({ _id: req.params.id });
-      res.redirect('/admin');
-    }
-  } catch (err) {
-    console.error(err);
-    return res.render('error/500');
-  }
-});
+router.delete('/:id', ensureAuth,controller.deleteGame);
 
 //Show single game
 // @route   GET /game/:id
-router.get('/:id', ensureAuth, async (req, res) => {
-  try {
-    let game = await Game.findById(req.params.id).populate('user').lean()
-
-    if (!game) {
-      return res.render('error/404')
-    }
-
-    if (game.user._id != req.user.id && game.status == 'private') {
-      res.render('error/404')
-    } else {
-      //console.log(game);
-      res.render('games/show', {
-        game,
-      })
-    }
-  } catch (err) {
-    console.error(err)
-    res.render('error/404')
-  }
-});
+router.get('/:id', ensureAuth, controller.showSingleGame);
 
 // User games
 // @route   GET /games/user/:userId
-router.get('/user/:userId', ensureAuth, async (req, res) => {
-  try {
-    const games = await Game.find({
-      user: req.params.userId,
-      status: 'active',
-    })
-      .populate('user')
-      .lean()
+router.get('/user/:userId', ensureAuth, controller.userGames)
 
-    res.render('games/index', {
-      games,
-    })
-  } catch (err) {
-    console.error(err)
-    res.render('error/500')
-  }
-})
-
-function saveCover(game, coverEncoded) {
-  if (coverEncoded == null) return
-  const cover = JSON.parse(coverEncoded)
-  if (cover != null && imageMimeTypes.includes(cover.type)) {
-    game.coverImage = new Buffer.from(cover.data, 'base64')
-    game.coverImageType = cover.type
-  }
-}
+// function saveCover(game, coverEncoded) {
+//   if (coverEncoded == null) return
+//   const cover = JSON.parse(coverEncoded)
+//   if (cover != null && imageMimeTypes.includes(cover.type)) {
+//     game.coverImage = new Buffer.from(cover.data, 'base64')
+//     game.coverImageType = cover.type
+//   }
+// }
 
 
 module.exports = router
