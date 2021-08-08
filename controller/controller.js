6191dc111efe4e008181d5355f7a66adf6c37864
Gaurav Sharma <gauravsharma10167@gmail.com>
2021-08-08 20:12:19 +0530

    update bux fix

--- controller/controller.js
+++ controller/controller.js
@@ -1,174 +1,172 @@
-const Game = require('../models/Game');
-const imageMimeTypes = ['image/jpeg', 'image/png', 'image/webp' ,'images/gif'];
+const Game = require("../models/Game");
+const imageMimeTypes = ["image/jpeg", "image/png", "image/webp", "images/gif"];
 
+addPage = (req, res) => {
+  res.render("games/add");
+};
 
-addPage =(req, res) => {
-    res.render('games/add');
+addForm = async (req, res) => {
+  let newgame = new Game({
+    name: req.body.name,
+    description: req.body.description,
+    status: req.body.status,
+    user: req.user.id,
+    link: req.body.link,
+  });
+  try {
+    saveCover(newgame, req.body.cover);
+    // req.body.user = req.user.id
+    // await Game.create(req.body)
+    newgame = await newgame.save();
+    res.redirect("/admin");
+  } catch (err) {
+    console.error(err);
+    res.render("error/form");
   }
+};
 
-addForm=async (req, res) => {
-    let newgame = new Game({
-      name: req.body.name,
-      description: req.body.description,
-      status: req.body.status,
-      user: req.user.id,
-      link: req.body.link
-    })
-    try {
-      saveCover(newgame, req.body.cover);
-      // req.body.user = req.user.id
-      // await Game.create(req.body)
-      newgame = await newgame.save();
-      res.redirect('/admin')
-    } catch (err) {
-      console.error(err)
-      res.render('error/form')
+showAllGames = async (req, res) => {
+  try {
+    const games = await Game.find({ status: "active" })
+      .populate("user")
+      .sort({ createdAt: "desc" })
+      .lean();
+
+    res.render("games/index", {
+      games,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
+};
+
+showEdit = async (req, res) => {
+  try {
+    let game = await Game.findOne({
+      _id: req.params.id,
+    });
+    game = game.toJSON();
+
+    if (!game) {
+      return res.render("error/404");
     }
-  }
 
-showAllGames= async (req, res) => {
-    try {
-      const games = await Game.find({ status: 'active' })
-        .populate('user')
-        .sort({ createdAt: 'desc' })
-        .lean();
-  
-      res.render('games/index', {
-        games,
+    if (game.user != req.user.id) {
+      res.redirect("/games");
+    } else {
+      res.render("games/edit", {
+        game,
       });
-    } catch (err) {
-      console.error(err);
-      res.render('error/500');
     }
+  } catch (err) {
+    console.error(err);
+    return res.render("error/500");
   }
+};
 
-  showEdit= async (req, res) => {
-    try {
-      let game = await Game.findOne({
-        _id: req.params.id,
-      })
-      game = game.toJSON();
-  
-      if (!game) {
-        return res.render('error/404');
-      }
-  
-      if (game.user != req.user.id) {
-        res.redirect('/games');
-      } else {
-        res.render('games/edit', {
-          game,
-        })
-      }
-    } catch (err) {
-      console.error(err);
-      return res.render('error/500');
+updateGame = async (req, res) => {
+  try {
+    let game = await Game.findById(req.params.id).lean();
+
+    if (!game) {
+      return res.render("error/404");
     }
-  }
+    if (game.user != req.user.id) {
+      res.redirect("/games");
+    } else {
+      story = await Game.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      saveCover(story, req.body.cover);
+      await story.save();
 
-  updateGame= async (req, res) => {
-    try {
-      let game = await Game.findById(req.params.id).lean()
-  
-      if (!game) {
-        return res.render('error/404')
-      }
-      if (game.user != req.user.id) {
-        res.redirect('/games')
-      } else {
-        story = await Game.findOneAndUpdate({ _id: req.params.id },
-          req.body
-      , {
-            new: true,
-            runValidators: true,
-          })
-  
-        res.redirect('/admin')
-      }
-    } catch (err) {
-      console.error(err)
-      return res.render('error/500')
+      res.redirect("/admin");
     }
+  } catch (err) {
+    console.error(err);
+    return res.render("error/500");
   }
+};
 
-  deleteGame= async (req, res) => {
-    try {
-      let game = await Game.findById(req.params.id).lean();
-  
-      if (!game) {
-        return res.render('error/404');
-      }
-  
-      if (game.user != req.user.id) {
-        res.redirect('/games');
-      } else {
-        await Game.remove({ _id: req.params.id });
-        res.redirect('/admin');
-      }
-    } catch (err) {
-      console.error(err);
-      return res.render('error/500');
+deleteGame = async (req, res) => {
+  try {
+    let game = await Game.findById(req.params.id).lean();
+
+    if (!game) {
+      return res.render("error/404");
     }
-  }
 
-  showSingleGame= async (req, res) => {
-    try {
-      let game = await Game.findById(req.params.id).populate('user');
-      game = game.toJSON();
-      console.log(game);
-      if (!game) {
-        return res.render('error/404')
-      }
-  
-      if (game.user._id != req.user.id && game.status == 'private') {
-        res.render('error/404')
-      } else {
-        //console.log(game);
-        res.render('games/show', {
-          game,
-        })
-      }
-    } catch (err) {
-      console.error(err)
-      res.render('error/404')
+    if (game.user != req.user.id) {
+      res.redirect("/games");
+    } else {
+      await Game.remove({ _id: req.params.id });
+      res.redirect("/admin");
     }
+  } catch (err) {
+    console.error(err);
+    return res.render("error/500");
   }
+};
 
-  userGames= async (req, res) => {
-    try {
-      const games = await Game.find({
-        user: req.params.userId,
-        status: 'active',
-      })
-        .populate('user')
-        .lean()
-  
-      res.render('games/index', {
-        games,
-      })
-    } catch (err) {
-      console.error(err)
-      res.render('error/500')
+showSingleGame = async (req, res) => {
+  try {
+    let game = await Game.findById(req.params.id).populate("user");
+    game = game.toJSON();
+    console.log(game);
+    if (!game) {
+      return res.render("error/404");
     }
-  }
 
-  
-  function saveCover(game, coverEncoded) {
-    if (coverEncoded == null) return
-    const cover = JSON.parse(coverEncoded)
-    if (cover != null && imageMimeTypes.includes(cover.type)) {
-      game.coverImage = new Buffer.from(cover.data, 'base64')
-      game.coverImageType = cover.type
+    if (game.user._id != req.user.id && game.status == "private") {
+      res.render("error/404");
+    } else {
+      //console.log(game);
+      res.render("games/show", {
+        game,
+      });
     }
+  } catch (err) {
+    console.error(err);
+    res.render("error/404");
   }
+};
 
-module.exports={
-    addPage,
-    addForm,
-    showAllGames,
-    showEdit,
-    updateGame,
-    deleteGame,
-    showSingleGame,
-    userGames
+userGames = async (req, res) => {
+  try {
+    const games = await Game.find({
+      user: req.params.userId,
+      status: "active",
+    })
+      .populate("user")
+      .lean();
+
+    res.render("games/index", {
+      games,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
+};
+
+function saveCover(game, coverEncoded) {
+  if (coverEncoded == null) return;
+  const cover = JSON.parse(coverEncoded);
+  if (cover != null && imageMimeTypes.includes(cover.type)) {
+    game.coverImage = new Buffer.from(cover.data, "base64");
+    game.coverImageType = cover.type;
+  }
 }
+
+module.exports = {
+  addPage,
+  addForm,
+  showAllGames,
+  showEdit,
+  updateGame,
+  deleteGame,
+  showSingleGame,
+  userGames,
+};
